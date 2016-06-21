de5e855423f949a719cfa7e62bcd0c50b0e342c3
Tseng <peter88850@gmail.com>
2016-06-21 16:34:56 +0800

    add 有點小失敗的 sticky header

--- JS/index.js
+++ JS/index.js
@@ -1,27 +1,26 @@
 $(function(){
  	
+	//nav//
+	
     $(".nav").on('click',function(){
-	 $(".nv-wrapper").toggle();
+	 $(".nv-wrapper").fadeToggle();
     });
-   
-   // 對每個 hottest-news 元素進行以下語法
-    $(".hottest-news").each(function(){
-        
-		var slides = $(this).find('.hottest-news-article'),
-            slideCount = slides.length,
-            currentIndex = 0;
-		
-		var nextIndex = (currentIndex +1) % slideCount;
 
-        function showNextSlide() {
-            slides.eq(currentIndex).fadeOut();
-            slides.eq(nextIndex).fadeIn();
-            currentIndex = nextIndex;
+	//stickymenu//
+	$(window).trigger('scroll');
+	
+    var headerOffsetTop = $("header").offset().top;
+ 
+	$(window).on('scroll', function() {
+        if($(window).scrollTop() > headerOffsetTop){
+            $("header").addClass('sticky');
+        } else {
+            $("header").removeClass('sticky');
         }
-		
-		 // 每秒執行 showNextSlide 函式
-        setInterval(showNextSlide, 3000);
-
-    });	
+    });
+	
+	//
+	
+	
 	
 });
